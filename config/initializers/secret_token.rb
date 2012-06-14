4b47899b7291eb0d1f9c36688d09930a1bbd7e6d
fir <firman.alqoumy@gmail.com>
2012-06-14 14:45:56 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hanifirman::Application.config.secret_token = 'b9dec2f185725f64557900cbdafc0973ab646db390e35127e8ea8e4b855c7365b0c4d93501831460680bb574ec789816c2c93eeb6932bc25206ec0db3af2eaa1'
